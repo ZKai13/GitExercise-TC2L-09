f1b9846c32f6594e9bcad3971b1424acedb3ab11
Daniel <Zhekai0513@gmail.com>
2024-08-27 22:53:51 +0800

    15%

--- Project/Assets/C##/Item Collect.cs
+++ Project/Assets/C##/Item Collect.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class ItemCollect : MonoBehaviour
 {
+    [SerializeField] private int melonCountToCollect = 3;
     private int Melon = 0;
+    public GameObject box2;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Melon"))
         {
             Destroy(collision.gameObject);
+            Melon++;
+            Debug.Log("Collected Melon: " + Melon);
+
+            if (Melon >= melonCountToCollect)  
+            {   
+                HandleThreeMelonsCollected();  
+            }      
         }
     }
+
+    private void HandleThreeMelonsCollected()  
+    {  
+       Debug.Log("Collected three melons!");  
+       Destroy(box2);
+    }  
 }
