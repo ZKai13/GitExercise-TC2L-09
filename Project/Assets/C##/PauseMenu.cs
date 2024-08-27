34185a005495dab4455aa4b3fa9e1159d8f5e91f
Dellmao <desmondyong312@gmail.com>
2024-08-27 21:18:00 +0800

    Options Panel in Main Menu

--- Project/Assets/C##/PauseMenu.cs
+++ Project/Assets/C##/PauseMenu.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using System.Threading.Tasks;
+
 
 
 public class PauseMenu : MonoBehaviour
 {
 
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] RectTransform pausePanelRect;
+    [SerializeField] float leftPosX, middlePosX;
+    [SerializeField] float tweenDuration;
+    [SerializeField] CanvasGroup canvasGroup;
 
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        PausePanelIntro();
     }
 
-    public void Resume()
+    public async void Resume()
     {
+        await PausePanelOutro();
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        
     }
 
     public void Restart()
@@ -32,5 +42,18 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
+
+    void PausePanelIntro()
+    {
+        canvasGroup.DOFade(1, tweenDuration).SetUpdate(true);
+        pausePanelRect.DOAnchorPosX(middlePosX, tweenDuration).SetUpdate(true);
+    }
+
+    async Task PausePanelOutro()
+    {
+        canvasGroup.DOFade(0, tweenDuration).SetUpdate(true);
+        await pausePanelRect.DOAnchorPosX(leftPosX, tweenDuration).SetUpdate(true).AsyncWaitForCompletion();
+    }
+
 
 }
