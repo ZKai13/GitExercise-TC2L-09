672fd14cec8a788dfdbeddcaf8fb506e1c69d4d2
Daniel <Zhekai0513@gmail.com>
2024-09-03 22:05:16 +0800

    Campfire

---
+++ Project/Assets/C##/Camp.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camp : MonoBehaviour
+{  
+    private bool isNearInteractable = false;  
+    public Health playerHealth;
+    public GameObject Square1;
+
+    void Update()  
+    {  
+        if (isNearInteractable && Input.GetKeyDown(KeyCode.P))  
+        {  
+            Interact();  
+        }  
+    }
+
+     private void Interact()
+    {  
+        if (playerHealth != null)  
+        {  
+            playerHealth.Heal(20);
+            Destroy(Square1);
+            Destroy(gameObject);
+        }  
+    }  
+
+    private void OnTriggerEnter2D(Collider2D other)  
+    {  
+        if (other.CompareTag("Player")) 
+        {  
+            isNearInteractable = true;  
+        }  
+    }  
+
+    private void OnTriggerExit2D(Collider2D other)  
+    {  
+        if (other.CompareTag("Player"))  
+        {  
+            isNearInteractable = false;  
+        }  
+    }  
+}
