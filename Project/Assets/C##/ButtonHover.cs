5ed4fb2b66074d3088457dc77903b2c74571f5ac
Dellmao <desmondyong312@gmail.com>
2024-09-15 20:41:16 +0800

    Updated Resolutions Menu

--- Project/Assets/C##/ButtonHover.cs
+++ Project/Assets/C##/ButtonHover.cs
@@ -8,55 +8,54 @@ using System.Collections.Generic; // If using TextMeshPro
 
 public class ButtonHoverDescription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public GameObject descriptionTextBox;  // The text box GameObject to show
-    public string descriptionText;         // The description to display
-    public Canvas parentCanvas;            // Reference to the parent canvas
+    public GameObject descriptionTextBox;  
+    public string descriptionText;         
+    public Canvas parentCanvas;            
 
-    private TextMeshProUGUI descriptionTextMeshPro;  // For TMP Text
-    private RectTransform descriptionRectTransform;  // RectTransform of the description box
+    private TextMeshProUGUI descriptionTextMeshPro;  
+    private RectTransform descriptionRectTransform;  
     
     Vector2 offset = new Vector2(150f, 60f); // Offset to move description slightly from mouse position
 
-    private bool isHovering = false; // Track if we're currently hovering over the button
+    private bool isHovering = false; 
 
     void Start()
     {
-        // Set the description text based on whether you're using TMP
+        
         if (descriptionTextBox.TryGetComponent(out TextMeshProUGUI tmpComponent))
         {
             descriptionTextMeshPro = tmpComponent;
         }
 
-        // Get the RectTransform of the description box to move it later
+       
         descriptionRectTransform = descriptionTextBox.GetComponent<RectTransform>();
 
-        // If parentCanvas is not assigned, try to find it
+       
         if (parentCanvas == null)
         {
             parentCanvas = GetComponentInParent<Canvas>();
         }
 
-        // Hide the description text box initially
         descriptionTextBox.SetActive(false);
     }
 
-    // Show the description when the cursor enters the button area
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
-        isHovering = true; // We're hovering over the button
+        isHovering = true; 
         ShowDescription();
     }
 
-    // Hide the description when the cursor exits the button area
+    
     public void OnPointerExit(PointerEventData eventData)
     {
-        isHovering = false; // No longer hovering
+        isHovering = false; 
         HideDescription();
     }
 
     void Update()
     {
-        // If hovering, update the position of the text box to follow the mouse
+   
         if (isHovering)
         {
             Vector2 mousePos;
@@ -66,12 +65,12 @@ public class ButtonHoverDescription : MonoBehaviour, IPointerEnterHandler, IPoin
                 parentCanvas.worldCamera, 
                 out mousePos);
 
-            // Update the position of the description text box, adding the offset
+ 
             descriptionRectTransform.anchoredPosition = mousePos + offset;
         }
     }
 
-    // Show the description text
+
     private void ShowDescription()
     {
         if (descriptionTextMeshPro != null)
@@ -83,7 +82,6 @@ public class ButtonHoverDescription : MonoBehaviour, IPointerEnterHandler, IPoin
         descriptionTextBox.SetActive(true);
     }
 
-    // Hide the description text
     private void HideDescription()
     {
         descriptionTextBox.SetActive(false);
--- Project/Assets/C##/ResolutionsMenu.cs
+++ Project/Assets/C##/ResolutionsMenu.cs
@@ -5,32 +5,30 @@ using System.Collections.Generic;
 public class ResolutionsMenu : MonoBehaviour
 {
     public Dropdown resolutionDropdown;
-    public Toggle fullscreenToggle;  // Added for fullscreen toggle
+    public Toggle fullscreenToggle;  
 
     private Resolution[] resolutions;
 
     void Start()
     {
-        // Get all available resolutions
+
         resolutions = Screen.resolutions;
 
-        // Clear any existing options in the dropdown
         resolutionDropdown.ClearOptions();
 
-        // Create a list to hold the resolution options as strings
         List<string> options = new List<string>();
 
-        // Populate the dropdown with available resolutions
+    
         foreach (Resolution resolution in resolutions)
         {
             string option = resolution.width + " x " + resolution.height + " @ " + resolution.refreshRate + "Hz";
             options.Add(option);
         }
 
-        // Add the options to the dropdown
+      
         resolutionDropdown.AddOptions(options);
 
-        // Set the current resolution and fullscreen state
+       
         int savedResolutionIndex = GetSavedResolutionIndex();
         resolutionDropdown.value = savedResolutionIndex;
         resolutionDropdown.RefreshShownValue();
@@ -38,37 +36,36 @@ public class ResolutionsMenu : MonoBehaviour
         bool isFullscreen = GetSavedFullscreenState();
         fullscreenToggle.isOn = isFullscreen;
 
-        // Apply the saved resolution and fullscreen state
+        
         ApplyResolution(savedResolutionIndex);
         Screen.fullScreen = isFullscreen;
 
-        // Add listeners for dropdown and toggle changes
+       
         resolutionDropdown.onValueChanged.AddListener(delegate { SetResolution(resolutionDropdown.value); });
         fullscreenToggle.onValueChanged.AddListener(delegate { SetFullscreen(fullscreenToggle.isOn); });
     }
 
-    // Get the index of the saved resolution
+    
     private int GetSavedResolutionIndex()
     {
-        // Default to 0 if no saved index is found
+        
         return PlayerPrefs.GetInt("ResolutionIndex", 0);
     }
 
-    // Get the saved fullscreen state
+    
     private bool GetSavedFullscreenState()
     {
-        // Default to true if no saved state is found
+       
         return PlayerPrefs.GetInt("Fullscreen", 1) == 1;
     }
 
-    // Method to set the resolution based on the selected dropdown option
     public void SetResolution(int resolutionIndex)
     {
         if (resolutionIndex >= 0 && resolutionIndex < resolutions.Length)
         {
             Resolution selectedResolution = resolutions[resolutionIndex];
             Screen.SetResolution(selectedResolution.width, selectedResolution.height, Screen.fullScreen);
-            // Save the selected resolution index
+           
             PlayerPrefs.SetInt("ResolutionIndex", resolutionIndex);
             PlayerPrefs.Save();
         }
@@ -78,16 +75,14 @@ public class ResolutionsMenu : MonoBehaviour
         }
     }
 
-    // Method to toggle fullscreen mode
+   
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
-        // Save the fullscreen state
         PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0);
         PlayerPrefs.Save();
     }
 
-    // Apply the resolution based on the saved index
     private void ApplyResolution(int resolutionIndex)
     {
         if (resolutionIndex >= 0 && resolutionIndex < resolutions.Length)
