b7d6691838827fe4e85c41a60c7ac9ab11e18138
Daniel <Zhekai0513@gmail.com>
2024-09-21 19:25:07 +0800

    Hide Camp

--- Project/Assets/C##/Camp.cs
+++ Project/Assets/C##/Camp.cs
@@ -8,11 +8,17 @@ public class Camp : MonoBehaviour
     private bool hasInteracted = false;  
     public Health playerHealth;
     public PotionScript potionScript;
+    public CanvasGroup campCanvasGroup;
+    public GameObject itemToHide;
+
+    public float fadeDuration = 1.0f;
 
     void Update()  
     {  
         if (isNearInteractable && Input.GetKeyDown(KeyCode.F) && !hasInteracted)  
         {  
+
+            StartCoroutine(FadeInAndInteract());
             Interact();
             if (!PlayerPrefs.HasKey("AhhYes"))
             {
@@ -24,6 +30,26 @@ public class Camp : MonoBehaviour
         }  
     }
 
+     private IEnumerator FadeInAndInteract()
+     {
+        yield return StartCoroutine(FadeCanvas(campCanvasGroup, 0, 1, fadeDuration)); 
+        Interact();
+        yield return new WaitForSeconds(2.0f); 
+
+        if (itemToHide != null)
+        {
+            var spriteRenderer = itemToHide.GetComponent<SpriteRenderer>();
+            if (spriteRenderer != null)
+            {
+                spriteRenderer.sortingLayerName = "New Layer 8"; // 修改为你需要的 Sorting Layer
+                Debug.Log("Item sorting layer changed to New Layer 8");
+            }
+        }
+
+        yield return StartCoroutine(FadeCanvas(campCanvasGroup, 1, 0, fadeDuration)); 
+    }
+
+
      private void Interact()
     {  
         if (playerHealth != null)  
@@ -32,7 +58,19 @@ public class Camp : MonoBehaviour
             potionScript.ResetPotionCount();
             hasInteracted = true;
         }  
-    }  
+    } 
+
+    private IEnumerator FadeCanvas(CanvasGroup canvasGroup, float startAlpha, float endAlpha, float duration)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = endAlpha; 
+    } 
 
     private void OnTriggerEnter2D(Collider2D other)  
     {  
